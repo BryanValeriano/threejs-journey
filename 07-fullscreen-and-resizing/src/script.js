0e7d53ce6f02b64b48eee4dc808ad18b8d66a1a1
BryanValeriano <bryan.b.valeriano@gmail.com>
2023-11-19 23:48:45 -0300

    add lil gui

--- 07-fullscreen-and-resizing/src/script.js
+++ 07-fullscreen-and-resizing/src/script.js
@@ -1,5 +1,23 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import gsap from 'gsap';
+import GUI from 'lil-gui';
+
+/**
+ * Debug
+ */
+const gui = new GUI({
+  width: 300,
+  title: 'Nice debug UI',
+  closeFolders: false,
+});
+// gui.close()
+// gui.hide()
+window.addEventListener('keydown', (event) => {
+  if (event.key == 'h') { gui.show(gui._hidden); }
+});
+
+const debugObject = {};
 
 /**
  * Base
@@ -13,26 +31,58 @@ const scene = new THREE.Scene();
 /**
  * Object
  */
+debugObject.color = '#a778d8';
 
-// const geometry = new THREE.BoxGeometry(1, 1, 1);
-
-// Create an empty BufferGeometry
-const geometry = new THREE.BufferGeometry();
-
-// Create 50 triangles (450 values)
-const count = 50;
-const positionsArray = new Float32Array(count * 3 * 3);
-for (let i = 0; i < count * 3 * 3; i += 1) {
-  positionsArray[i] = (Math.random() - 0.5) * 4;
-}
-
-// Create the attribute and name it 'position'
-const positionsAttribute = new THREE.BufferAttribute(positionsArray, 3);
-geometry.setAttribute('position', positionsAttribute);
-
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: '#a778d8', wireframe: true });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
+
+const cubeTweaks = gui.addFolder('Awesome cube');
+// cubeTweaks.close()
+
+cubeTweaks
+  .add(mesh.position, 'y')
+  .min(-3)
+  .max(3)
+  .step(0.01)
+  .name('elevation');
+
+cubeTweaks
+  .add(mesh, 'visible');
+
+cubeTweaks
+  .add(material, 'wireframe');
+
+cubeTweaks
+  .addColor(debugObject, 'color')
+  .onChange(() => {
+    material.color.set(debugObject.color);
+  });
+
+debugObject.spin = () => {
+  gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+};
+cubeTweaks
+  .add(debugObject, 'spin');
+
+debugObject.subdivision = 2;
+cubeTweaks
+  .add(debugObject, 'subdivision')
+  .min(1)
+  .max(20)
+  .step(1)
+  .onFinishChange(() => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivision,
+      debugObject.subdivision,
+      debugObject.subdivision,
+    );
+  });
 
 /**
  * Sizes
@@ -41,23 +91,6 @@ const sizes = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-
-/**
- * Renderer
- */
-const renderer = new THREE.WebGLRenderer({
-  canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-
-/**
- * Camera
- */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
-camera.position.z = 3;
-scene.add(camera);
 
 window.addEventListener('resize', () => {
   // Update sizes
@@ -74,34 +107,35 @@ window.addEventListener('resize', () => {
 });
 
 /**
- * Fullscreen
+ * Camera
  */
-window.addEventListener('dblclick', () => {
-  const fullscreenElement = document.fullscreenElement || document.webkitFullscreenElement;
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100);
+camera.position.x = 1;
+camera.position.y = 1;
+camera.position.z = 2;
+scene.add(camera);
 
-  if (!fullscreenElement) {
-    if (canvas.requestFullscreen) {
-      canvas.requestFullscreen();
-    } else if (canvas.webkitRequestFullscreen) {
-      canvas.webkitRequestFullscreen();
-    }
-  } else if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen();
-  }
-});
 // Controls
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 /**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+/**
  * Animate
  */
-// const clock = new THREE.Clock();
+const clock = new THREE.Clock();
 
 const tick = () => {
-  // const elapsedTime = clock.getElapsedTime();
+  const elapsedTime = clock.getElapsedTime();
 
   // Update controls
   controls.update();
